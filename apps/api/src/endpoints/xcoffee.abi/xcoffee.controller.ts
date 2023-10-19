895cfec240ee9c28de107e192a6ea122774b9b5e
Marius Oprea <dev.omr95@gmail.com>
2023-10-19 19:40:27 +0300

    Add NativeAuthGuard

--- apps/api/src/endpoints/xcoffee.abi/xcoffee.controller.ts
+++ apps/api/src/endpoints/xcoffee.abi/xcoffee.controller.ts
@@ -1,29 +1,34 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Address } from '@multiversx/sdk-core/out';
 import { XcoffeeService } from './xcoffee.service';
-// import { NativeAuth } from '@multiversx/sdk-nestjs-auth';
+import { NativeAuth, NativeAuthGuard } from '@multiversx/sdk-nestjs-auth';
 
 @Controller('xcoffee')
 @ApiTags('xcoffee.abi-with-cache')
-// @UseGuards(NativeAuthGuard)
-// @ApiBearerAuth()
+@UseGuards(NativeAuthGuard)
+@ApiBearerAuth()
 export class XcoffeeController {
   constructor(private readonly xcoffeeService: XcoffeeService) {}
 
-  @Get('/subscription-duration/:userAddress/:creatorAddress')
+  /**
+   * Get how many seconds the subscription has until expires.
+   * @param userAddress Wallet address of the subscriber
+   * @param creatorAddress Wallet address of the creator
+   * @returns Number of seconds
+   */
+  @Get('/subscription-duration/:creatorAddress')
   @ApiResponse({
     status: 200,
     description: 'Returns one example',
   })
   async getSubscriptionDuration(
-    @Param('userAddress') userAddress: string,
+    @NativeAuth('address') userAddress: string,
     @Param('creatorAddress') creatorAddress: string,
   ): Promise<{
     status: string;
     remainingTime?: number;
   }> {
-    console.log;
     return await this.xcoffeeService.getSubscriptionDuration(
       Address.fromString(userAddress),
       Address.fromString(creatorAddress),
